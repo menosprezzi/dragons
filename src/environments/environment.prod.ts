92a0ef7d6adb3ce7102fd880629dbcc986ec1537
Guilherme Prezzi <guilhermeprezzi1997@gmail.com>
2019-09-05 15:16:06 -0300

    fix: Using https in prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   hmr: false,
-  dragonsApi: 'http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon'
+  dragonsApi: 'https://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon'
 };
