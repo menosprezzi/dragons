e87cae0cc0fc77fbab35c0738860cd0e6d0de2cf
Guilherme Prezzi <guilhermeprezzi1997@gmail.com>
2019-09-05 13:47:29 -0300

    test(dragons-edit/dragons.service): Updating tests

--- src/app/dragons/dragons.service.spec.ts
+++ src/app/dragons/dragons.service.spec.ts
@@ -1,9 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DragonsService } from './dragons.service';
+import { environment } from '../../environments/environment';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('DragonsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: [ { provide: 'APP_ENVIRONMENT', useValue: environment } ]
+    });
+  });
 
   it('should be created', () => {
     const service: DragonsService = TestBed.get(DragonsService);
